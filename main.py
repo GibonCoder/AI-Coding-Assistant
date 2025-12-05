a204e88b5ae0ed9b2707d4356634a2f725d5aaef
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-12-05 23:15:35 +0100

    Add necessary imports

---
+++ main.py
@@ -0,0 +1,6 @@
+from google.adk.agents import Agent
+from google.adk.models.google_llm import Gemini
+from google.adk.runners import Runner
+from google.adk.sessions import InMemorySessionService
+from google.adk.tools import AgentTool, FunctionTool, google_search
+from google.adk.code_executors import BuiltInCodeExecutor
